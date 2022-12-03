277923faef71546088fdb5599be15e256d326fce
Luca Ionescu <29058596+lucaionescu@users.noreply.github.com>
2022-12-03 19:36:13 +0100

    concise day 02 test

--- test/aoc/day_02_test.clj
+++ test/aoc/day_02_test.clj
@@ -2,18 +2,16 @@
   (:require [clojure.test :refer [deftest is]]
             [aoc.day-02 :refer [part-1 part-2]]))
 
+(def x [["A" "Y"]
+        ["B" "X"]
+        ["C" "Z"]])
+
 (deftest part-1-test
-  (let [x [["A" "Y"]
-           ["B" "X"]
-           ["C" "Z"]]
-        solution (part-1 x)
+  (let [solution (part-1 x)
         expected 15]
     (is (= solution expected))))
 
 (deftest part-2-test
-  (let [x [["A" "Y"]
-           ["B" "X"]
-           ["C" "Z"]]
-        solution (part-2 x)
+  (let [solution (part-2 x)
         expected 12]
     (is (= solution expected))))
