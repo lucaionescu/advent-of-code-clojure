c3b54ac1db6360fef94ad470378b7bdbbc8983f2
Luca Ionescu <29058596+lucaionescu@users.noreply.github.com>
2023-03-10 13:41:17 +0100

    day 12 solution

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject aoc "0.1.0-SNAPSHOT"
   :description "Advent of Code 2022"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.priority-map "1.1.0"]]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ test/aoc/day_12_test.clj
@@ -0,0 +1,17 @@
+(ns aoc.day-12-test
+  (:require [clojure.test :refer [deftest is]]
+            [aoc.day-12 :refer [find-position part-1 part-2]]))
+
+(def rows ["Sabqponm"
+           "abcryxxl"
+           "accszExk"
+           "acctuvwj"
+           "abdefghi"])
+
+(def end_pos (find-position rows \E))
+
+(deftest part-1-test
+  (is (= (part-1 rows end_pos) 31)))
+
+(deftest part-2-test
+  (is (= (part-2 rows end_pos) 29)))
